ad9f3b014e16a4d77ba2da1f49dc82498021873c
Cenk Öztürk <ccenkozturrk@outlook.com>
2025-01-20 20:07:33 +0300

    Created a new DbContext.

--- AdvancedLINQApiShowcase/Program.cs
+++ AdvancedLINQApiShowcase/Program.cs
@@ -1,6 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+using AdvancedLINQApiShowcase.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
